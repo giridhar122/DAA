2b90352095043db0f30e738860f95ec993e833a9
Giridhar <161578859+giridhar122@users.noreply.github.com>
2025-09-20 01:57:43 +0530

    Add files via upload

---
+++ insertion sort.py
@@ -0,0 +1,12 @@
+a=[8,3,2,6,2,6]
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > key:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+    return a
+
+print(insertion_sort(a))
